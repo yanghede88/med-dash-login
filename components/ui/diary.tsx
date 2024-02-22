b93521bcc9f6a27047f66eaa707c2738dc2dec77
Jessica Fong <76975581+j6fong@users.noreply.github.com>
2024-02-22 15:52:24 -0800

    Diary Entries files

---
+++ components/ui/diary.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { DatePickerWithPresets } from '@/components/ui/datepick'
+
+import entries from '@/SampleEntries.json';
+
+
+export function CarouselDemo() {
+
+  return (
+    <Carousel className="w-full max-w-xl">
+      <CarouselContent>
+        {entries.map(entry => (
+          <CarouselItem key={entry.timestamp}>
+            <div className="p-1">
+              <Card>
+                <CardHeader>
+                    <CardTitle className="flex justify-center p-6"><DatePickerWithPresets givenDate={new Date(entry.timestamp)}/></CardTitle>
+                </CardHeader>
+                <CardContent className="flex aspect-square p-6">
+                  <span>{entry.entry}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
